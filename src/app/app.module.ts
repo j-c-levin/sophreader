816e32a9eb5aa2af8f6fc77ba88f6f66b6b90c2a
Joshua Levin <joshua.c.levin@gmail.com>
2018-07-14 23:36:09 +0100

    accordion working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,14 +9,14 @@ import { NgxsModule } from '@ngxs/store';
 import { ReaderState } from './state/reader.state';
 import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
 import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
-import { FeedItemComponent } from './components/feed-item/feed-item.component';
+import { MatExpansionModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
     ReaderComponent,
-    FeedListComponent,
-    FeedItemComponent
+    FeedListComponent
   ],
   imports: [
     BrowserModule,
@@ -25,7 +25,9 @@ import { FeedItemComponent } from './components/feed-item/feed-item.component';
       ReaderState
     ]),
     NgxsReduxDevtoolsPluginModule.forRoot(),
-    NgxsLoggerPluginModule.forRoot()
+    NgxsLoggerPluginModule.forRoot(),
+    MatExpansionModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
